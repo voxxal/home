e1a2d087b4f333a2e7db6108daee2190976b42c1
voxal <aidenhohoho@gmail.com>
2022-08-07 10:50:17 -0700

    shitty model code

---
+++ model.js
@@ -0,0 +1,74 @@
+import { html, render } from "https://unpkg.com/lit-html?module";
+import { classMap } from "https://unpkg.com/lit-html/directives/class-map.js?module";
+
+const skillLevels = ["Beginner", "Competent", "Proficient", "Expert", "Mastered"];
+
+const skillBar = (skill, color) =>
+  html` <div class="skill-bar">
+    ${new Array(4).fill("").map(
+      (_, i) => html`<div class="skill-segment">
+        <div
+          class="skill-inner-segment"
+          style="width: ${Math.max(Math.min((skill / 25 - i) * 100, 100), 0)}%; --color: ${color}"
+        ></div>
+      </div>`
+    )}
+  </div>`;
+
+let modelOpen = false;
+let currentModelData = {};
+
+const techModel = (data) => html` <div class="${classMap({ model: true, open: modelOpen })}">
+  <div class="model-content">
+    <div class="model-left">
+      <h3 class="model-title">${data.title}</h3>
+      <p class="model-description">${data.description}</p>
+    </div>
+    <div class="model-right">
+      ${skillBar(data.skill, data.color)}
+      <p class="skill-level" style="--color: ${data.color}">
+        ${skillLevels[Math.floor(data.skill / 25)]}
+      </p>
+    </div>
+    <span class="model-close" @click=${closeModel}>&times;</span>
+  </div>
+</div>`;
+
+const closeModel = (e) => {
+  modelOpen = false;
+  render(techModel(currentModelData), document.body);
+};
+
+const openTechModel = (data) => () => {
+  modelOpen = true;
+  currentModelData = data;
+  render(techModel(data), document.body);
+};
+
+const rustModelData = {
+  title: "Rust",
+  description: "Rust has to be my favorite language. ",
+  skill: 65,
+  color: "var(--rust-brand)",
+};
+
+const typescriptModelData = {
+  title: "Typescript",
+  description:
+    "I probably use Typescript the most, since I mostly do web development these days. There are a few parts of Typescript that I haven't explored yet though.",
+  skill: 80,
+  color: "var(--typescript-brand)",
+};
+
+render(
+  html` I love programming langauges. My main languages are
+    <span class="rust-color open-model" @click=${openTechModel(rustModelData)}>Rust</span>
+    and
+    <span class="typescript-color open-model" @click=${openTechModel(typescriptModelData)}
+      >Typescript</span
+    >.`,
+  document.getElementById("main-technologies")
+);
+
+window.techModel = techModel;
+window.render = render;
