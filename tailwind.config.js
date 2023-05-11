cebfdd38f424ffe8719f2148be07767cd3a7dda1
voxal <aidenhohoho@gmail.com>
2023-05-10 23:18:25 -0700

    remove any mention of mdsvex

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const plugin = require("tailwindcss/plugin");
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,svelte,ts,md}"],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {
       colors: {
