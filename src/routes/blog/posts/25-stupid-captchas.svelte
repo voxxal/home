154dd4f9d28565747389104770aba6ec765bc650
voxal <aidenhohoho@gmail.com>
2024-07-29 14:47:07 -0700

    blog post

---
+++ src/routes/blog/posts/25-stupid-captchas.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+  export const metadata = {
+    title: "25 stupid captchas",
+    published: "2024-07-29",
+  };
+</script>
+
+<p>i made a silly game</p>
+<p>it is out right now</p>
+<p>i am not a robot</p>
+
+<p><a href="/25-stupid-captchas/">play here</a></p>
