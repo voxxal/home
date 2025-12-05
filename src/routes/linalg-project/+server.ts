0a80f64062aa758e81a5dadc7f88d50cc1eb7571
voxal <58456527+voxxal@users.noreply.github.com>
2025-12-04 16:55:21 -0800

    fix proxy test

--- src/routes/linalg-project/+server.ts
+++ src/routes/linalg-project/+server.ts
@@ -1,4 +1,18 @@
+    
 export async function GET({ fetch, url }) {
   url.pathname = "/linalg-project/index.html";
-  return await fetch(url);
+  
+  const response = await fetch(url);
+
+  const newHeaders = new Headers(response.headers);
+  
+  newHeaders.delete('content-encoding');
+  newHeaders.delete('content-length');
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: newHeaders
+  });
 }
+
