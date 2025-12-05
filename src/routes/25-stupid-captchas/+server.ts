6c667a4feb0cab552590584b398aa2a0a61aa3f8
voxal <58456527+voxxal@users.noreply.github.com>
2025-12-04 18:07:51 -0800

    fix proxy for all

--- src/routes/25-stupid-captchas/+server.ts
+++ src/routes/25-stupid-captchas/+server.ts
@@ -3,7 +3,21 @@ import { redirect } from "@sveltejs/kit";
 export const trailingSlash = "always";
 
 export async function GET({ fetch, url }) {
-  if (url.pathname[url.pathname.length - 1] != "/") throw redirect(308, "/25-stupid-captchas/");
+  if (!url.pathname.endsWith("/")) {
+    throw redirect(308, "/25-stupid-captchas/");
+  }
+
   url.pathname = "/25-stupid-captchas/index.html";
-  return await fetch(url);
+  
+  const response = await fetch(url);
+
+  const newHeaders = new Headers(response.headers);
+  newHeaders.delete("content-encoding");
+  newHeaders.delete("content-length");
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: newHeaders,
+  });
 }
